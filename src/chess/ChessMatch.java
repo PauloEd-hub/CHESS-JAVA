2e04f31269b57c481994155c7f78085b32762777
PauloEd-hub <paulo2002.cavalcante@hotmail.com>
2021-03-04 22:25:54 -0300

    Inserting the first pieces.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
 
     }
 
@@ -20,4 +24,11 @@ public class ChessMatch {
         }
         return mat;
     }
-}
+    private void initialSetup() {
+        board.placeHolder(new Rook(board, Color.WHITE), new Position(2,1));
+        board.placeHolder(new King(board, Color.BLACK), new Position(0,4));
+        board.placeHolder(new King(board, Color.BLACK), new Position(7,4));
+        }
+    }
+
+
