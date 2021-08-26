8c786723fe50434c9bd85b8aaa0651f34b9db978
PauloEd-hub <paulo2002.cavalcante@hotmail.com>
2021-08-25 23:55:18 -0300

    Especial move - castling

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -95,6 +95,23 @@ public class ChessMatch {
             piecesOnTheBoard.remove(capturedPiece);
             capturedPieces.add(capturedPiece);
         }
+        // #specialMove castling kingside rook
+        if(p instanceof King && target.getColumn() == source.getColumn() + 2) {
+            Position sourceT = new Position(source.getRow(), source.getColumn() + 3);
+            Position targetT = new Position(source.getRow(), source.getColumn() + 1);
+            ChessPiece rook = (ChessPiece)board.removePiece((sourceT));
+            board.placePiece(rook, targetT);
+            rook.increaseMoveCount();
+        }
+        // #specialMove castling queenside rook
+        if(p instanceof King && target.getColumn() == source.getColumn() - 2) {
+            Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+            Position targetT = new Position(source.getRow(), source.getColumn() - 1);
+            ChessPiece rook = (ChessPiece)board.removePiece((sourceT));
+            board.placePiece(rook, targetT);
+            rook.increaseMoveCount();
+        }
+
         return capturedPiece;
     }
 
@@ -108,6 +125,27 @@ public class ChessMatch {
             capturedPieces.remove(capturedPiece);
             piecesOnTheBoard.add(capturedPiece);
         }
+        // #specialMove castling kingside rook
+        if(p instanceof King && target.getColumn() == source.getColumn() + 2) {
+            Position sourceT = new Position(source.getRow(), source.getColumn() + 3);
+            Position targetT = new Position(source.getRow(), source.getColumn() + 1);
+            ChessPiece rook = (ChessPiece)board.removePiece((targetT));
+            board.placePiece(rook, sourceT);
+            rook.decreaseMoveCount();
+        }
+        // #specialMove castling queenside rook
+        if(p instanceof King && target.getColumn() == source.getColumn() - 2) {
+            Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+            Position targetT = new Position(source.getRow(), source.getColumn() - 1);
+            ChessPiece rook = (ChessPiece)board.removePiece((targetT));
+            board.placePiece(rook, sourceT);
+            rook.decreaseMoveCount();
+        }
+
+
+
+
+
     }
 
 
